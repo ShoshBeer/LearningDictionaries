9091c297164921495188865851de8d906e86cf23
ShoshBeer <shoshanabeer@gmail.com>
2023-02-17 20:07:20 -0500

    improve dictionary processing, get related words

--- processWordDump.py
+++ processWordDump.py
@@ -6,7 +6,7 @@ def makeLearningDict(filename, wordsToExclude, minWordLength = 3, POStoInclude =
 
   roughList = []
   with open(filename, "r", encoding="utf-8") as f:
-    # I can also grab the language code from any word, might make things easier to do that than input language manually
+    # Can grab the language code from any word and return it to use in the frequency processing rather than manual input
     print(f'Reading {totalLines} total lines.')
     tenPercent = totalLines//10
     currentLine = 0
@@ -22,31 +22,56 @@ def makeLearningDict(filename, wordsToExclude, minWordLength = 3, POStoInclude =
       if ' ' in data["word"] or len(data["word"]) < minWordLength or data["pos"] not in POStoInclude:
         # Skip multi-word phrases
         # By default, only take nouns, verbs, adjectives
-        pass
-
-      elif data["senses"][0].get("raw_glosses") == None:
-        #idk what this takes out, the senses array has "raw_glosses" and "glosses" and they look like the same thing and seem like definitions
-        #for some reason, some don't have "raw_glosses" so this just prevents an error when grabbing the definition
-        #later I can look through the ones without it and check what's being skipped and if the definition is stored elsewhere
-        pass
-
-      elif any([x in data["senses"][0]["raw_glosses"][0].casefold() for x in wordsToExclude]):
-        # Use this to filter out conjugations and other irrelevant words
-        # Definitions in other languages are in English so might hardcode list (at least some of these types of words are also in other language defs)
-        pass
+        continue
 
       else:
         if len(roughList) == 0 or data["word"] != roughList[-1]["word"]:
-          roughList.append({"word": data["word"], "definitions": [], "synonyms": []})
+          roughList.append({"word": data["word"], "definitions": [], "related words": []})
+
         for sense in range(len(data["senses"])): 
-          #this could be skipped if the synonyms from educalingo are sufficient, but will keep for now
-          roughList[-1]["definitions"].append([data["pos"], data["senses"][sense]["raw_glosses"][0]])
+          if "raw_glosses" in data["senses"][sense]:
+            if not any([x in data["senses"][sense]["raw_glosses"][0].casefold() for x in wordsToExclude]):
+              roughList[-1]["definitions"].append([data["pos"], data["senses"][sense]["raw_glosses"][0]])
+
+          elif "glosses" in data["senses"][sense]:
+            if not any([x in data["senses"][sense]["glosses"][0].casefold() for x in wordsToExclude]):
+              roughList[-1]["definitions"].append([data["pos"], data["senses"][sense]["glosses"][0]])
+
           if "synonyms" in data["senses"][sense]:
             for synonym in data["senses"][sense]["synonyms"]:
-              roughList[-1]["synonyms"].append(synonym["word"])
+              if synonym["word"] not in roughList[-1]["word"] and roughList[-1]["word"] not in synonym["word"] and not any(synonym["word"] in similarWords for similarWords in roughList[-1]["related words"]):
+                roughList[-1]["related words"].append(["synonym", synonym["word"]])
+          
+          if "hypermyns" in data["senses"][sense]:
+            for hypernym in data["senses"][sense]["hypermyns"]:
+              if hypernym["word"] not in roughList[-1]["word"] and roughList[-1]["word"] not in hypernym["word"] and not any(hypernym["word"] in similarWords for similarWords in roughList[-1]["related words"]):
+                roughList[-1]["related words"].append(["hypernym", hypernym["word"]])
+
+          if "hyponyms" in data["senses"][sense]:
+            for hyponym in data["senses"][sense]["hyponyms"]:
+              if hyponym["word"] not in roughList[-1]["word"] and roughList[-1]["word"] not in hyponym["word"] and not any(hyponym["word"] in similarWords for similarWords in roughList[-1]["related words"]):
+                roughList[-1]["related words"].append(["hyponym", hyponym["word"]])
+
+          if "meronyms" in data["senses"][sense]:
+            for meronym in data["senses"][sense]["meronyms"]:
+              if meronym["word"] not in roughList[-1]["word"] and roughList[-1]["word"] not in meronym["word"] and not any(meronym["word"] in similarWords for similarWords in roughList[-1]["related words"]):
+                roughList[-1]["related words"].append(["meronym", meronym["word"]])
+
+          if "antonyms" in data["senses"][sense]:
+            for antonym in data["senses"][sense]["antonyms"]:
+              if antonym["word"] not in roughList[-1]["word"] and roughList[-1]["word"] not in antonym["word"] and not any(antonym["word"] in similarWords for similarWords in roughList[-1]["related words"]):
+                roughList[-1]["related words"].append(["antonym", antonym["word"]])
+
+          if "related" in data["senses"][sense]:
+            for related in data["senses"][sense]["related"]:
+              if related["word"] not in roughList[-1]["word"] and roughList[-1]["word"] not in related["word"] and not any(related["word"] in similarWords for similarWords in roughList[-1]["related words"]):
+                roughList[-1]["related words"].append(["related", related["word"]])
+
+        if len(roughList[-1]["definitions"]) == 0:
+          # These are some really weird and obscure words, so just going to filter these out (words without any glosses or raw_glosses in any sense for any POS)
+          roughList.pop(-1)
 
   return roughList
-
 
 if __name__ == "__main__":
   excluded = [
@@ -57,7 +82,8 @@ if __name__ == "__main__":
             "misspelling", "alternative form", "alternative spelling", "alternative letter", 
             "script ", "greek", "phonetic"
           ]
-  testData = makeLearningDict('en', "kaikki.org-dictionary-English-words.json", excluded)
+
+  roughList = makeLearningDict("kaikki.org-dictionary-English-words.json", excluded)
 
   with open('test_new_function_more_defs.json', 'w', encoding='utf-8') as f:
-    json.dump(testData, f)
+    json.dump(roughList, f)
