135e1434db47877dcacf36651de7585c900776b8
ShoshBeer <shoshanabeer@gmail.com>
2023-02-27 14:53:48 -0500

    change draft doc to dict instead of list

--- process_test/utils/processWordDump.py
+++ process_test/utils/processWordDump.py
@@ -4,7 +4,7 @@ import wordfreq as wf
 def makeLearningDict(filename, wordsToExclude, minWordLength = 3, POStoInclude = 'noun verb adj'):
   totalLines = sum(1 for line in open(filename, "r", encoding="utf-8")) # to print progress
 
-  roughList = []
+  roughDict = {}
   with open(filename, "r", encoding="utf-8") as f:
     langCode = json.loads(f.readline())["lang_code"]
     print(f'Reading {totalLines} total lines.')
@@ -25,31 +25,31 @@ def makeLearningDict(filename, wordsToExclude, minWordLength = 3, POStoInclude =
         continue
 
       else:
-        if len(roughList) == 0 or data["word"] != roughList[-1]["word"]:
-          roughList.append({"word": data["word"], "definitions": [], "related words": []})
+        if data["word"] not in roughDict:
+          roughDict[data["word"]] = {"word": data["word"], "definitions": [], "related words": []}
 
         for sense in range(len(data["senses"])): 
           if "raw_glosses" in data["senses"][sense]:
             if not any([x in data["senses"][sense]["raw_glosses"][0].casefold() for x in wordsToExclude]):
-              roughList[-1]["definitions"].append([data["pos"], data["senses"][sense]["raw_glosses"][0]])
+              roughDict[data["word"]]["definitions"].append([data["pos"], data["senses"][sense]["raw_glosses"][0]])
 
           elif "glosses" in data["senses"][sense]:
             if not any([x in data["senses"][sense]["glosses"][0].casefold() for x in wordsToExclude]):
-              roughList[-1]["definitions"].append([data["pos"], data["senses"][sense]["glosses"][0]])
+              roughDict[data["word"]]["definitions"].append([data["pos"], data["senses"][sense]["glosses"][0]])
 
           for relationship in ["synonyms", "hypermyns", "hyponyms", "meronyms", "antonyms", "related"]:
             if relationship in data["senses"][sense]:
               for relatedWord in data["senses"][sense][relationship]:
-                if relatedWord["word"].casefold() not in roughList[-1]["word"] and \
-                   roughList[-1]["word"] not in relatedWord["word"].casefold() and \
-                   not any(relatedWord["word"].casefold() in similarWord[1].casefold() for similarWord in roughList[-1]["related words"]):
-                      roughList[-1]["related words"].append([relationship[:-1], relatedWord["word"]])
+                if relatedWord["word"].casefold() not in roughDict[data["word"]]["word"] and \
+                   roughDict[data["word"]]["word"] not in relatedWord["word"].casefold() and \
+                   not any(relatedWord["word"].casefold() in similarWord[1].casefold() for similarWord in roughDict[data["word"]]["related words"]):
+                      roughDict[data["word"]]["related words"].append([relationship[:-1], relatedWord["word"]])
 
-        if len(roughList[-1]["definitions"]) == 0:
+        if len(roughDict[data["word"]]["definitions"]) == 0:
           # These are some really weird and obscure words, so just going to filter these out (words without any glosses or raw_glosses in any sense for any POS)
-          roughList.pop(-1)
+          del roughDict[data["word"]]
 
-  return roughList, langCode
+  return roughDict, langCode
 
 if __name__ == "__main__":
   excluded = [
@@ -66,7 +66,7 @@ if __name__ == "__main__":
             "script ", "greek", "phonetic"
           ]
 
-  [roughList, langCode] = makeLearningDict("kaikki.org-dictionary-English-words.json", excluded)
+  [roughDict, langCode] = makeLearningDict("kaikki.org-dictionary-English-words.json", excluded)
 
-  with open('test_new_function_more_defs.json', 'w', encoding='utf-8') as f:
-    json.dump(roughList, f)
+  with open('en_draft_dict.json', 'w', encoding='utf-8') as f:
+    json.dump(roughDict, f)
